3f0d889c346789628a22de2dfea901fc02f6d382
Peterlee <peterlee0127@gmail.com>
2012-08-12 15:31:11 +0800

    add comment

--- delete_unused_InternetAccounts.sh
+++ delete_unused_InternetAccounts.sh
@@ -1,9 +1,15 @@
 #! /bin/sh
 cd /System/Library/InternetAccounts
+# Delete QQ
 sudo rm -rf QQ.iaplugin/
+# Delete 126
 sudo rm -rf 126.iaplugin/
+# Delete 163
 sudo rm -rf 163.iaplugin/
+# Delete Tudou
 sudo rm -rf Tudou.iaplugin/
+# Delete Weibo
 sudo rm -rf Weibo.iaplugin/
+# Youku
 sudo rm -rf Youku.iaplugin/
 clear
